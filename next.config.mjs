b2213211f20a78645d10fe07eb1ad6cf6847a4c7
unknown <maxime.guigra@gmail.com>
2025-01-09 23:25:09 +0100

    feat: initial config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url); // Chemin absolu du fichier courant
+const __dirname = path.dirname(__filename);       // Répertoire contenant le fichier
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+  },
+};
 
 export default nextConfig;

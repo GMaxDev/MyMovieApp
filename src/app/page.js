ac300c0072ff05e917d74482736524038cdfd1a7
unknown <maxime.guigra@gmail.com>
2025-01-13 15:07:33 +0100

    feat: add like client component button for test

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import MediaCard from "@/components/Header/media-card/MediaCard";
 import styles from "./page.module.css";
+import PostList from "@/components/PostList";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.main}>
         <MediaCard mediaId={10}/>
+        <PostList />
       </div>
     </div>
   );
---
+++ src/components/Like.js
@@ -0,0 +1,11 @@
+"use client"
+
+export default function Like() {
+  return (
+    <div>
+      <div>
+        <button onClick={() => console.log("like")}>like</button>
+      </div>
+    </div>
+  );
+}
